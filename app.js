a13d031f3624a154446a5db5615c09648466aa0b
yacax <yacax@yandex.ru>
2023-05-28 15:00:52 +0300

    Fix logger module import error

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const logger = require('./utils/logger');
+const { logger } = require('./utils/logger');
 
 const app = express();
 const { PORT = 3000 } = process.env;
